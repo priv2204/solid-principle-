0ec6f1ed7068fe62c63c79a989401e69a657a9cd
vipul <priyanshubajpai96@gmail.com>
2025-03-26 22:51:09 +0530

    add logger in develop

--- solid-principles-example-main/src/main/java/com/javatechie/solid/dip/CreditCard.java
+++ solid-principles-example-main/src/main/java/com/javatechie/solid/dip/CreditCard.java
@@ -3,6 +3,7 @@ package com.javatechie.solid.dip;
 public class CreditCard implements BankCard{
 
     public void doTransaction(long amount){
+        System.out.println("running in dev branch");
         System.out.println("payment using Credit card");
     }
 }
