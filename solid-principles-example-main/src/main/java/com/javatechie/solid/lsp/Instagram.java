ec39513d76b7428925d66a74d6a0540c494e8b7f
priv2204 <93195459+priv2204@users.noreply.github.com>
2025-03-26 19:15:12 +0530

    Add files via upload

---
+++ solid-principles-example-main/src/main/java/com/javatechie/solid/lsp/Instagram.java
@@ -0,0 +1,20 @@
+package com.javatechie.solid.lsp;
+
+public class Instagram extends SocialMedia {
+//doesnot follow lsp as groupVideoCall is not having supported by instagram
+    public void chatWithFriend() {
+
+    }
+
+    public void publishPost(Object post) {
+
+    }
+
+    public void sendPhotosAndVideos() {
+
+    }
+
+    public void groupVideoCall(String... users) {
+//not applicable
+    }
+}
